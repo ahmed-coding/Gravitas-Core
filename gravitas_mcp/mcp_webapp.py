f1ca3a8a4a960e2bf5e3a1e7d028d685e17d7060
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2026-02-11 08:02:56 +0300

    update uvicorn port

--- gravitas_mcp/mcp_webapp.py
+++ gravitas_mcp/mcp_webapp.py
@@ -43,4 +43,4 @@ starlette_app = Starlette(
 
 
 if __name__ == "__main__":
-    uvicorn.run(starlette_app, host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run(starlette_app, host="0.0.0.0", port=8765, log_level="info", )
